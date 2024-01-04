0f5ab4c5805bbdf28331dae9a5d748bec0b5bf19
Franson <franson.naz@gmail.com>
2024-01-04 05:29:12 +0000

    Created all configuration files

--- 09-ec2-instance.tf
+++ 09-ec2-instance.tf
@@ -1,1 +1,16 @@
+# AWS EC2 Instance Terraform Module
+# - EC2 Instance that will be created in VPC Public Subnet
+module "ec2_public" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.0.0"  
 
+  name = "${local.name}-ec2-Host"
+  ami                    = data.aws_ami.amzlinux2.id
+  instance_type          = var.instance_type
+  key_name               = var.instance_keypair
+  #monitoring             = true
+  subnet_id              = module.vpc.public_subnets[0]
+  vpc_security_group_ids = [module.public_ec2_sg.security_group_id]
+  
+  tags = local.common_tags
+}
